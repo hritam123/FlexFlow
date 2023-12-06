ccc124b7fa0ebae1506d6cca7f7db95a7c7e607f
Hritam <hritamsaha@gmail.com>
2023-12-06 14:39:28 +0530

    Landing Page Done

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -52,7 +52,7 @@ const HomePage = () => {
             bg-background
           "
           >
-            Get Cypress Free
+            Get FlexFlow Free
           </Button>
         </div>
         <div
